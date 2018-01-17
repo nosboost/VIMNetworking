cf2e8b67a43851a1ef7c66b42c83cb96eb8122cd
Krystian Malicki <krystian.malicki@tigerspike.com>
2018-01-17 12:00:04 +0100

    Updated project to the latest pod structure.
    Removed unneeded static lib and added example project.

---
+++ Example/VIMNetworkingSDK/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  VIMNetworkingSDK
+//
+//  Created by Krystian Malicki on 01/17/2018.
+//  Copyright (c) 2018 Krystian Malicki. All rights reserved.
+//
+
+@import UIKit;
+#import "KMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KMAppDelegate class]));
+    }
+}
